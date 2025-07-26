c0653c5a6aefbb94bc0becd14a50b86b5c742984
mulham <mulhamalzain@gmail.com>
2025-07-26 15:07:01 +0300

    banking

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import { useForm } from 'react-hook-form';
 import { zodResolver } from '@hookform/resolvers/zod';
 import { z } from 'zod';
 
-// Schemas
+
 const recordSchema = z.object({
   name: z.string().min(1, { message: 'Name is required' }),
   amount: z
